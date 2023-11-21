cc7c2ae0bd7b36609192dc8483dcb1431a883c03
damaoooo <damaoooo@163.com>
2023-11-21 19:18:52 +0300

    self implementation

---
+++ Assignment4/Cache.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+from Utils import *
+
+class CacheMappingType(Enum):
+    DirectMapping = 1
+    FullyAssociative = 2
+    SetAssociative = 3
+
---
+++ Assignment4/Utils.py
@@ -0,0 +1,7 @@
+from enum import IntEnum
+
+class Size(IntEnum):
+    B = 1
+    KB = 1024
+    MB = 1024 ** 2
+    GB = 1024 ** 3
